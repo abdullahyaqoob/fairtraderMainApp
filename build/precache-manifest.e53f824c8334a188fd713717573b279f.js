be60f68abc19d2724b34136ec7e0f8750de40d87
abdullahyaqoob <65088172+abdullahyaqoob@users.noreply.github.com>
2022-04-05 12:11:47 +0500

    Add files via upload

---
+++ build/precache-manifest.e53f824c8334a188fd713717573b279f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "85015a3d58a8a763e4db",
+    "url": "/static/css/main.210344df.chunk.css"
+  },
+  {
+    "revision": "85015a3d58a8a763e4db",
+    "url": "/static/js/main.85015a3d.chunk.js"
+  },
+  {
+    "revision": "c807bdaba51b23f2e266",
+    "url": "/static/css/1.65df51bf.chunk.css"
+  },
+  {
+    "revision": "c807bdaba51b23f2e266",
+    "url": "/static/js/1.c807bdab.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7f191f5377df4a93735a324140ea317c",
+    "url": "/index.html"
+  }
+];
