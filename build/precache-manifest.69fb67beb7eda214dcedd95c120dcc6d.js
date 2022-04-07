16c88fd87a4043e29364a5a6dd654219430e063f
abdullahyaqoob <65088172+abdullahyaqoob@users.noreply.github.com>
2022-04-07 21:33:05 +0500

    Add files via upload

---
+++ build/precache-manifest.69fb67beb7eda214dcedd95c120dcc6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e354958072489f2ce01",
+    "url": "/static/css/main.3a208dee.chunk.css"
+  },
+  {
+    "revision": "0e354958072489f2ce01",
+    "url": "/static/js/main.0e354958.chunk.js"
+  },
+  {
+    "revision": "4a10a1f79fa639a97e55",
+    "url": "/static/css/1.8579f485.chunk.css"
+  },
+  {
+    "revision": "4a10a1f79fa639a97e55",
+    "url": "/static/js/1.4a10a1f7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "526e1e00f37978d0d5066ef32571d987",
+    "url": "/static/media/walletContentLogo.526e1e00.png"
+  },
+  {
+    "revision": "386d0d3415657ea0678f0420de54cb8e",
+    "url": "/index.html"
+  }
+];
