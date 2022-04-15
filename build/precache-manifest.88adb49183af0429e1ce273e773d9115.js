8e315973c7a22ed78f8171871431ef3a8ec30193
abdullahyaqoob <65088172+abdullahyaqoob@users.noreply.github.com>
2022-04-15 16:15:36 +0500

    Add files via upload

---
+++ build/precache-manifest.88adb49183af0429e1ce273e773d9115.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f289c6df7f6d612fb71",
+    "url": "/static/css/main.edad26a6.chunk.css"
+  },
+  {
+    "revision": "4f289c6df7f6d612fb71",
+    "url": "/static/js/main.4f289c6d.chunk.js"
+  },
+  {
+    "revision": "d95e9255a356ff990a6d",
+    "url": "/static/css/1.48b1bfb1.chunk.css"
+  },
+  {
+    "revision": "d95e9255a356ff990a6d",
+    "url": "/static/js/1.d95e9255.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "17570aeee5840f08038e78be1b72f0a3",
+    "url": "/static/media/invoiceDummyPDF1.17570aee.png"
+  },
+  {
+    "revision": "526e1e00f37978d0d5066ef32571d987",
+    "url": "/static/media/walletContentLogo.526e1e00.png"
+  },
+  {
+    "revision": "c8c515c7c409e0de31c61e1dc81a2e14",
+    "url": "/static/media/invoiceDummyPDF2.c8c515c7.pdf"
+  },
+  {
+    "revision": "abd0c36fc23cccc8927a7802d25866f9",
+    "url": "/index.html"
+  }
+];
